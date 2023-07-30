bbc86d85edf36b995024b891a4e05273e0f54f37
Kendrick <kmg3795@yahoo.com>
2023-07-30 15:25:30 -0500

    added skills section to html.index

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const today = new Date()
+const thisYear = today.getFullYear()
+const footer = document.querySelector("body > footer")
+
+const copyright = document.createElement('p')
+    copyright.innerText = "Kendrick Goedecke " + thisYear
+
+footer.appendChild(copyright)
+
+const skills = [
+    "JavaScript",
+    "HTML",
+    "Video Production",
+]
+
+const skillsSection = document.querySelector("#skills")
+const skillsList = document.querySelector("#skills > ul")
+
+for (i = 0; i < 3; i++) {
+    const skill = document.createElement('li')
+    skill.innerText = skills[i]
+    skillsList.appendChild(skill)
+}
